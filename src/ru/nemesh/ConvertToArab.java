f3ccd90c42bc6cfe09456fa01ce8d937d8c5c4be
Konstantin <nemeshkv@gmail.com>
2022-08-29 10:29:32 +0500

    start2+interface+interface

--- src/ru/nemesh/ConvertToArab.java
+++ src/ru/nemesh/ConvertToArab.java
@@ -3,16 +3,16 @@ package ru.nemesh;
 public class ConvertToArab implements Arab {
     public static int converta(String a) {
 
-        if (a.equals(1)) return 1;
-        if (a.equals(2)) return 2;
-        if (a.equals(3)) return 3;
-        if (a.equals(4)) return 4;
-        if (a.equals(5)) return 5;
-        if (a.equals(6)) return 6;
-        if (a.equals(7)) return 7;
-        if (a.equals(8)) return 8;
-        if (a.equals(9)) return 9;
-        if (a.equals(10)) return 10;
+        if (a.equals("1")) return 1;
+        if (a.equals("2")) return 2;
+        if (a.equals("3")) return 3;
+        if (a.equals("4")) return 4;
+        if (a.equals("5")) return 5;
+        if (a.equals("6")) return 6;
+        if (a.equals("7")) return 7;
+        if (a.equals("8")) return 8;
+        if (a.equals("9")) return 9;
+        if (a.equals("10")) return 10;
         return 0;
     }
 }
