6f528913ea1f86ec8f28c9ff5ee3a1f05cd300ee
Konstantin <nemeshkv@gmail.com>
2022-10-20 20:15:17 +0500

    new prod20102022

--- src/ru/nemesh/Convert.java
+++ src/ru/nemesh/Convert.java
@@ -2,21 +2,6 @@ package ru.nemesh;
 
 public class Convert {
 
-    static String checkRom(String a) {
-
-            if (a.equals("I")) return ("I");     //проверяем в строке римская или нет
-            if (a.equals("II")) return ("II");
-            if (a.equals("III")) return ("III");
-            if (a.equals("IV")) return ("IV");
-            if (a.equals("V")) return ("V");
-            if (a.equals("VI")) return ("VI");
-            if (a.equals("VII")) return ("VII");
-            if (a.equals("VIII")) return ("VIII");
-            if (a.equals("IX")) return ("IX");
-            if (a.equals("X")) return ("X");
-            System.out.println("Ошибка! Вы ввели символ или букву, необходимо ввести арабские или римские цифры!");
-            return a;
-        }
     static int convertToArab(String a) {
 
         if (a.equals("I")) return 1;  //конвертируем в римские
