c4a04f054ed8cb97ee9eaee2d343f424add79609
Konstantin <nemeshkv@gmail.com>
2022-11-18 12:52:08 +0500

    new prod18112022

--- src/ru/nemesh/Arab.java
+++ src/ru/nemesh/Arab.java
@@ -3,21 +3,21 @@ package ru.nemesh;
 public class Arab implements Calculator{
 
     @Override
-    public int plus(String a, String b) { return (Convert.convertToArab(a) + Convert.convertToArab(b)); }   //поведение с интерфейсом
+    public int plus(String a, String b) { return (Convert.convertToInt(a) + Convert.convertToInt(b)); }   //поведение с интерфейсом
 
     @Override
     public int minus(String a, String b) {
-        return (Convert.convertToArab(a) - Convert.convertToArab(b));
+        return (Convert.convertToInt(a) - Convert.convertToInt(b));
     }
 
     @Override
     public int multiply(String a, String b) {
-        return (Convert.convertToArab(a) * Convert.convertToArab(b));
+        return (Convert.convertToInt(a) * Convert.convertToInt(b));
     }
 
     @Override
     public int divide(String a, String b) {
-        return (Convert.convertToArab(a) / Convert.convertToArab(b));
+        return (Convert.convertToInt(a) / Convert.convertToInt(b));
     }
 
 }
--- src/ru/nemesh/Convert.java
+++ src/ru/nemesh/Convert.java
@@ -17,6 +17,20 @@ public class Convert {
         if (a.equals("X")) return b = 10;
         return Integer.parseInt(a);
     }
+    static int convertToInt(String a) {
+        int b;
+        if (a.equals("1")) return b = 1;
+        if (a.equals("2")) return b = 2;
+        if (a.equals("3")) return b = 3;
+        if (a.equals("4")) return b = 4;
+        if (a.equals("5")) return b = 5;
+        if (a.equals("6")) return b = 6;
+        if (a.equals("7")) return b = 7;
+        if (a.equals("8")) return b = 8;
+        if (a.equals("9")) return b = 9;
+        if (a.equals("10")) return b = 10;
+        return Integer.parseInt(a);
+    }
 
     static String convertToRom(int a) {
         String b = String.valueOf(a);
