9367d3bdc8be2bff6f93cab9825f67146cb1da9b
Konstantin <nemeshkv@gmail.com>
2022-12-06 23:17:45 +0500

    new prod06122022

--- src/ru/nemesh/Convertresult.java
+++ src/ru/nemesh/Convertresult.java
@@ -7,22 +7,51 @@ import java.util.stream.Collectors;
 
 public enum RomanNumeral1 {
 
-    I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), CM(900), M(1000);
-    private int b;
+    I("I",1), IV("IV",4), V("V",5), IX("IX",9), X("IX",10), XL("XL",40), L("L",50), XC("XC",90), C("C",100), CD("CD",400), D("D",500), CM("CM",900), M("M",1000);
 
-    RomanNumeral1(int b) {
-        this.b = b;
+    private int value;
+    private String key;
+
+    RomanNumeral1(String key, int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;}
+    public String getKey() {
+        return key;
+        int toInt(String key) {
+            for (RomanNumeral1 i: this.values())
+                if (i.getKey.equals(key))
+                    return i.getValue();
+
+            return "";
+        }
     }
 
-    public int getB(String a) { // 1.получили строку например String a = VI
+
+
+
+ //   private int b;
+
+//    RomanNumeral1(int b) {
+// //       this.b = b;
+//    }
+//
+//    public int getB(String a) {
+//        try {
+//            Double.parseDouble(a);
+//            if(a.equals(b)) return b;
+//        } catch (NumberFormatException e) {
+//            return calc;
+//        }
+        // 1.получили строку например String a = VI
         // 2.читаем enum c лево на право, находим самое первое число которое больше VI, это IX (9) и запоминаем которое меньшев в диапазоне, это V
         // 3.сраниваем и отнимаем от 9 наше число 6, получается 3 - тут видимо сравнение как раз с десятками надо делать, правильно?
         // 4.опять сравниваем полученное 3 с enum: но сначало с методом где десятки определяем какая строка (это III), далее сравнива с enum если не найдено снова применяем первый метод (ищем минимум и максимум), это между I и IV, вычитаем от большего и получаем I
         // 5. добавляем его I к запомниному V и получается VI
         //
 
-        
-        return B;
 
+        return 0;
     }
 }
